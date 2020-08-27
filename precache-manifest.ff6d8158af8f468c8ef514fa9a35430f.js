d3b6bbaa4b3bde7d94ee6ceed3d283a429b27506
DouglasFeliphe <douglasfeliphe@hotmail.com>
2020-08-27 01:16:15 -0300

    Updates

---
+++ precache-manifest.ff6d8158af8f468c8ef514fa9a35430f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d263fbb3fd840c4a636de5f735fd7d9c",
+    "url": "/Ecoleta_Web/index.html"
+  },
+  {
+    "revision": "9b6128773205a3fed026",
+    "url": "/Ecoleta_Web/static/css/main.cb7d8325.chunk.css"
+  },
+  {
+    "revision": "6a5340ceb121c361ff6c",
+    "url": "/Ecoleta_Web/static/js/2.52d37b34.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/Ecoleta_Web/static/js/2.52d37b34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b6128773205a3fed026",
+    "url": "/Ecoleta_Web/static/js/main.bcc396d7.chunk.js"
+  },
+  {
+    "revision": "a43cbc4b7d244d431695",
+    "url": "/Ecoleta_Web/static/js/runtime-main.defa11dd.js"
+  },
+  {
+    "revision": "fe2283e1f17af7f6d2889b5692937968",
+    "url": "/Ecoleta_Web/static/media/home-background.fe2283e1.svg"
+  },
+  {
+    "revision": "47c4581a1dc536c1ba57fddafd6671b9",
+    "url": "/Ecoleta_Web/static/media/logo.47c4581a.svg"
+  }
+]);
